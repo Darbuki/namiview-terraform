86ae4bebdf61e2ff619ba941ae19e0df3502e369
Dar Levy <darl293@gmail.com>
2026-04-10 22:08:16 +0000

    added access to user

--- envs/prod/eu-west-1/eks.tf
+++ envs/prod/eu-west-1/eks.tf
@@ -13,8 +13,33 @@ module "eks" {
 
   # Access
   endpoint_public_access       = true
-  endpoint_public_access_cidrs = ["172.225.187.6/32"]
+  endpoint_public_access_cidrs = ["0.0.0.0/0"]
   enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    ci = {
+      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/namiview-terraform-ci"
+      policy_associations = {
+        cluster_admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+    dar = {
+      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/darNamiview"
+      policy_associations = {
+        cluster_admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
 
   # EKS managed add-ons
   addons = {
--- envs/prod/eu-west-1/main.tf
+++ envs/prod/eu-west-1/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "namiview-prod-bucket" {
   bucket = "namiview-prod-bucket"
 }
