c825d33b8cce64bd9820bfc29405f045c0edcf8a
Dar Levy <darl293@gmail.com>
2026-04-11 10:34:08 +0000

    added loab balancer with irsa

---
+++ envs/prod/eu-west-1/alb.tf
@@ -0,0 +1,50 @@
+# ACM certificate for EKS services
+resource "aws_acm_certificate" "eks" {
+  domain_name               = "eks.namiview.com"
+  subject_alternative_names = ["*.eks.namiview.com"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# IRSA role for AWS Load Balancer Controller
+module "alb_controller_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name                              = "${var.cluster_name}-alb-controller"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+# AWS Load Balancer Controller
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  values = [yamlencode({
+    clusterName = var.cluster_name
+    region      = var.region
+    vpcId       = module.vpc.vpc_id
+
+    serviceAccount = {
+      create = true
+      name   = "aws-load-balancer-controller"
+      annotations = {
+        "eks.amazonaws.com/role-arn" = module.alb_controller_irsa.iam_role_arn
+      }
+    }
+  })]
+
+  depends_on = [module.eks]
+}
--- envs/prod/eu-west-1/outputs.tf
+++ envs/prod/eu-west-1/outputs.tf
@@ -5,3 +5,18 @@ output "github_actions_role_arn" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.namiview-prod-bucket.bucket
 }
+
+output "acm_certificate_arn" {
+  value = aws_acm_certificate.eks.arn
+}
+
+output "acm_dns_validation_records" {
+  description = "DNS records to create in Cloudflare for ACM validation"
+  value = {
+    for dvo in aws_acm_certificate.eks.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+}
