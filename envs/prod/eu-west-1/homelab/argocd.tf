0d829e1a657e57572adf09e40dbf29bd12c17667
Dar Levy <darl293@gmail.com>
2026-05-14 11:49:57 +0000

    Update ArgoCD Helm chart version to 9.5.14 for improved features and fixes

--- envs/prod/eu-west-1/homelab/argocd.tf
+++ envs/prod/eu-west-1/homelab/argocd.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "9.5.0"
+  version    = "9.5.14"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
 
   values = [file("${path.module}/argocd-values.yaml")]
